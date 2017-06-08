1e381ddef00938d0f71649d618b86c6f3a036a5d
iaforek <arkadiusz@jaworski.pl>
2017-06-08 11:46:02 +0100

    Added connection string.

--- src/main/java/com/example/demo/RateController.java
+++ src/main/java/com/example/demo/RateController.java
@@ -18,11 +18,15 @@ public class RateController {
 	@Value("${tollstart}")
 	String tollstart;
 	
+	@Value("${connstring}")
+	String connstring;
+	
 	@RequestMapping("/rate")
 	public String getRate(Model m) {
 		m.addAttribute("rateamount", rate);
 		m.addAttribute("lanes", lanecount);
 		m.addAttribute("tollstart", tollstart);
+		m.addAttribute("connstring", connstring);
 		
 		// Name of the view.
 		return "rateview";
